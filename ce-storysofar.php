9069980ab39744699cbb1a39788c4c8f5bed73a7
dreadfulgate <jaehling@gmx.net>
2018-06-29 17:00:40 +0200

    Initial commit

---
+++ ce-storysofar.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Plugin Name: Comic Easel - The Story So Far
+ * Plugin URI: http://comiceasel.org
+ * Description: Add-on to Comic Easel, adds a "story so far" meta box, shortcode and widget.
+ * Author: Max Vähling
+ * Author URI: http://dreadfulgate.de
+ * Version: 0.1.0
+ */
+
+// Meta-Box 
+
+function ce_story_metabox() {
+	add_meta_box( 
+		'ce_storysofar',
+		'The Story So Far',
+		'ce_storybox',
+		'comic',
+		'normal',
+		'high' );
+}
+
+add_action( 'add_meta_boxes', 'ce_story_metabox' );
+  
+function ce_storybox ($post) { 
+	wp_nonce_field( 'basename( __FILE__ )', 'do_the_nonce' );
+	$ce_storysofar = get_post_meta( $post->ID, 'ce_storysofar', true );					
+	echo '<textarea rows="3" name="ce_storysofar" style="width:100%;">'. $ce_storysofar .'</textarea>';
+	echo "<p>Add a story summary for readers to catch up quickly. You can display the summary in a sidebar using the 'Comic Easel - The Story so far' widget or in your comic post using the [storysofar] shortcode. (Feel free to change the headline by adding a 'title' tag to the shortcode.)</p>";
+}
+ 
+function save_story_meta( $post_id ) {   
+	// Pre-game: Check if nonce is set
+ if ( ! isset( $_POST['do_the_nonce'] ) ) {
+        return $post_id;
+	}
+    if ( ! wp_verify_nonce( $_POST['do_the_nonce'], 'basename( __FILE__ )' ) ) {
+        return $post_id;
+	} 
+	// Check that the logged in user has permission to edit this post
+    if ( ! current_user_can( 'edit_post' ) ) {
+        return $post_id;
+	}
+	// Game: 
+	if ($_POST['ce_storysofar']) {
+		update_post_meta( 
+				$post_id, 
+				"ce_storysofar", 
+				strip_tags($_POST['ce_storysofar'])
+		);
+	}
+}
+
+add_action( 'save_post', 'save_story_meta' );
+
+
+// Shortcode
+
+function ce_shortcode_so_far($atts) { 
+	global $post;
+	$summary = get_post_meta( $post->ID, 'ce_storysofar', true );
+	$att = shortcode_atts( array( 'title' => 'The Story so far:' ), $atts );		
+	if ( $summary ) {
+		$storysofar = '<div>
+							<h3>'. esc_html__($att[ 'title' ], 'sofar' )  .'</h3>
+							<p>'. $summary .'</p>
+						</div>';
+	}
+	
+	echo $storysofar;
+}
+	
+add_shortcode( 'storysofar', 'ce_shortcode_so_far' );
+
+ 
+// Widget
+
+class CE_StorySoFar extends WP_Widget {
+
+	function __construct () { 
+		$widget_options = array( 'classname' 	=> 'CE_StorySoFar',
+							'description' 	=> 'Spoilers!' );
+		parent::__construct( 'ce_storysofar', 'Comic Easel - The Story So Far', $widget_options );
+	}
+
+	function widget( $args, $instance ) { 
+		global $post;
+		$summary = get_post_meta( $post->ID, 'ce_storysofar', true );
+		if ( $summary ) {
+			echo $args[ 'before_widget' ];
+			if ( $instance[ 'with_title' ] ) {
+				echo $args[ 'before_title' ] . $instance[ 'title' ] . $args[ 'after_title' ] ;
+				}
+			echo '<p>'. $summary .'</p>'; 
+			echo $args['after_widget'];
+		}
+	}
+		
+	function form($instance) {
+		$defaults= array('with_title' => true,
+					'title'     => 'The Story So Far:' );
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		?>	
+			<p>
+				<label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+				<input name = "<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance[ 'title' ]); ?>" />	
+			</p><p>
+				<label for="<?php echo $this->get_field_id( 'with_title' ); ?>">Show Title</label>
+				<input type="checkbox" name="<?php echo $this->get_field_name( 'with_title' ); ?>" value="true" <?php checked( isset( $instance[ 'with_title' ]) ? $instance[ 'with_title' ] : 0); ?> />
+			</p>
+		<?php
+	}
+	
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance[ 'with_title' ] = $new_instance[ 'with_title' ] ? true : false;
+		$instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
+		return $instance;
+	}	
+}
+
+function ce_register_story_widgets() { 
+	register_widget( CE_StorySoFar ); 
+}
+	
+add_action( 'widgets_init', 'ce_register_story_widgets' );	
---
+++ ce-thisstorywidget.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Plugin Name: Comic Easel - This Story Widget
+ * Plugin URI: http://comiceasel.org
+ * Description: Add-on to Comic Easel, collects some story (chapter) info and a link to the first page into a widget.
+ * Author: Max Vähling
+ * Author URI: http://dreadfulgate.de
+ * Version: 0.1.0
+ */
+
+function ce_get_first_in_chapter() {
+	global $post;	
+	$chapter = get_the_terms( $post->ID, 'chapters' );
+	if (is_array($chapter)) { 
+		$chapter = reset($chapter); 
+	} else { 
+		return; 
+	}
+	if (is_object( $chapter ) ) {
+		$child_args = array( 
+				'numberposts' => 1, 
+				'post_type'   => 'comic',
+				'orderby' 	  => 'post_date', 
+				'order' 	  => 'ASC', 
+				'post_status' => 'publish', 
+				'chapters' 	  => $chapter->slug 
+				);				
+		$qcposts = get_posts( $child_args );
+		if ( is_array( $qcposts ) ) {
+			$qcposts = reset( $qcposts );
+			return get_permalink( $qcposts->ID );
+		}
+	}
+	return false;
+}
+
+
+class CE_ThisStory extends WP_Widget {
+
+	function __construct () { 
+		$widget_options = array( 'classname'   => 'CE_ThisStory',
+							'description' => 'Displays custom story info for the current comic' );
+		parent::__construct( 'ce_thisstory', 'Comic Easel - This Story Widget', $widget_options );
+	}
+
+	function widget( $args, $instance ) { 
+		$terms = get_the_terms( $post, 'chapters' );
+		if ( $terms ) { 
+			foreach ( $terms as $term ) { 
+				if ( ( get_term_children( $term->id, 'chapters' ) == 0 ) || ( ( get_term_children( $term->id, 'chapters' ) > 0 ) && ( count( $terms ) == 1 ) ) ) 
+				{
+					$storyline = $term->name; 
+					$synopsis = $term->description; 
+					$storylink = get_term_link( $term->slug, 'chapters');
+				}
+			}
+			$first_comic = ce_get_first_in_chapter( $post->ID );
+			$this_permalink = get_permalink( $post->ID );
+			if ( ( $synopsis ) || ( ( $term->count > 1 ) && ( $first_comic !== $this_permalink ) ) || ( $instance['show-anyway'] ) )
+			{
+				echo $args[ 'before_widget' ]. $args[ 'before_title' ]; 
+				if ( ! $instance[ 'archive' ] ) {
+					echo $storyline;
+				} else {
+					echo '<a href="'. $storylink .'">'. $storyline . '</a>';
+				}
+				echo  $args[ 'after_title' ] . '<div class="comic-post-info">';
+				if ( $instance[ 'synopsis' ] ) {
+					echo $synopsis; 
+				}
+				if ( ( $instance[ 'firstlink' ] ) && ( $first_comic !== $this_permalink ) ) { 
+					echo '<p><a href="'. $first_comic .'">'. $instance[ 'firstline' ] .'</a></p>'; }
+				echo '</div>';
+				echo $args[ 'after_widget' ];
+			}
+		}		
+	}
+	
+	function form ($instance) { 
+		$defaults = array ( 	'firstlink'	  => true,
+						'firstline'	  => 'Read from the beginning',	
+						'synopsis'	  => true,
+						'archive' 	  => true,
+						'show-anyway' => true
+					);		
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		?>
+			<p>
+				<label for="<?php echo $this->get_field_id('archive'); ?>">Link title to story archive</label>
+				<input type="checkbox" name="<?php echo $this->get_field_name('archive'); ?>" value="true" <?php checked(isset($instance['archive']) ? $instance['archive'] : 0); ?> />
+			</p><p>
+				<label for="<?php echo $this->get_field_id('firstlink'); ?>">Link to first page</label>
+				<input type="checkbox" name="<?php echo $this->get_field_name('firstlink'); ?>" value="true" <?php checked(isset($instance['firstlink']) ? $instance['firstlink'] : 0); ?> />
+			</p><p>
+				<label for="<?php echo $this->get_field_id('firstline'); ?>">Link text</label>
+				<input name = "<?php echo $this->get_field_name('firstline'); ?>" type="text" value="<?php echo esc_attr($instance['firstline']); ?>" />	
+			</p><p>
+				<label for="<?php echo $this->get_field_id('synopsis'); ?>">Include story description</label>
+				<input type="checkbox" name="<?php echo $this->get_field_name('synopsis'); ?>" value="true" <?php checked(isset($instance['synopsis']) ? $instance['synopsis'] : 0); ?> />
+			</p><p>
+				<label for="<?php echo $this->get_field_id('show-anyway'); ?>">Always show widget, even if it would only show the title and this is the only page in that story anyway</label>
+				<input type="checkbox" name="<?php echo $this->get_field_name('show-anyway'); ?>" value="true" <?php checked(isset($instance['show-anyway']) ? $instance['show-anyway'] : 0); ?> />
+			</p>	
+	
+		<?php 
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance[ 'archive' ] 		= $new_instance[ 'archive' ] ?	true : false;
+		$instance[ 'synopsis' ] 	= $new_instance[ 'synopsis' ] ? true : false;
+		$instance[ 'firstlink' ] 	= $new_instance[ 'firstlink' ] ? true : false;
+		$instance[ 'show-anyway' ] 	= $new_instance[ 'show-anyway' ] ? true : false;
+		$instance[ 'firstline' ] 	= strip_tags( $new_instance[ 'firstline' ] );
+		return $instance;
+	}	
+}
+
+function ce_register_widgets() { 
+	register_widget( CE_ThisStory ); 
+}
+
+add_action( 'widgets_init', 'ce_register_widgets' );
+
+
+// Bonus functions
+/*
+add_filter ('display_posts_shortcode_args', 'list_comic_chapters');
+
+function list_comic_chapters() {
+	$atts['chapter'] = '';
+	$chapter  = sanitize_text_field( $atts['chapter'] );
+		if( $chapter !== false ) {
+		if ( ($post_type == 'comic') && ( !empty( $chapter ) )) {
+			if( 'current' == $chapter ) {
+				global $post;
+				$chapter = $chapter = get_the_terms( $post->ID, 'chapters' );
+			}
+		}
+	}
+	$args['chapter'] = $chapter;	
+}
+*/
